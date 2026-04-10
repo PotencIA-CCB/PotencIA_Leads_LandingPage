18f2feb4153f98203aef334d811a87d8922ce7d2
Carlos Ortiz <carlosor3000@gmail.com>
2026-04-10 08:19:34 -0500

    Changed: Add new link for conection to microsoft  booking

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const BOOKING_URL =
-  "https://outlook.office.com/book/ConsultorioIAPotencia@camarabaq.org.co/s/QjoGRR4GKkaZOr2d9kGq2A2?ismsaljsauthenabled";
+  "https://outlook.office.com/book/Reservasprueba@camarabaq.org.co/?ismsaljsauthenabled";
